4470ebdbe2e6b9940735c2d0447c802249130336
Owen <owen.debiasio@gmail.com>
2025-11-23 18:33:57 -0500

    Implement cd and cwd functions for directory management

---
+++ src/fs.rs
@@ -0,0 +1,29 @@
+use std::env::{current_dir, set_current_dir};
+
+use crate::ENV;
+
+pub fn cd(dir: &str) {
+    let dir = match dir {
+        "~" | "$HOME" => ENV.home.clone(),
+        _ if dir.starts_with("~/") => format!("{}/{}", ENV.home, &dir[2..]),
+        _ if dir.starts_with("$HOME/") => format!("{}/{}", ENV.home, &dir[6..]),
+        _ => dir.to_string(),
+    };
+    if let Err(e) = set_current_dir(&dir) {
+        eprintln!("cd: {dir}: {e}");
+    }
+}
+
+pub fn cwd() -> String {
+    let cwd = current_dir()
+        .ok()
+        .and_then(|p| p.to_str().map(String::from))
+        .unwrap_or("~".into());
+    if cwd == ENV.home {
+        "~".into()
+    } else if let Some(r) = cwd.strip_prefix(&(ENV.home.clone() + "/")) {
+        format!("~/{r}")
+    } else {
+        cwd
+    }
+}
