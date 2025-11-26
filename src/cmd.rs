2bf87a0a18d4d4c70054f29603993dbea0c11889
Owen <owen.debiasio@gmail.com>
2025-11-26 12:52:07 -0500

    Enhance command execution with error handling
    Refactor command execution to use output handling and improved error reporting.

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,6 +1,6 @@
 use std::process::{Command, Stdio};
 
-use crate::fs::cd;
+use crate::{configs, fs::cd};
 
 pub fn run(c: &str) {
     let mut p = c.split_whitespace();
@@ -8,11 +8,23 @@ pub fn run(c: &str) {
         cd(p.next().unwrap_or("/"));
         return;
     }
-    let _ = Command::new("bash")
+
+    let output = Command::new(configs::load("commands", "shell", "bash"))
         .arg("-c")
         .arg(c)
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .status();
+        .stderr(Stdio::piped())
+        .output();
+
+    match output {
+        Ok(o) => {
+            if let Ok(err) = String::from_utf8(o.stderr) {
+                if err.contains("command not found") {
+                    println!("minsh: {c} command not found");
+                }
+            }
+        }
+        Err(e) => eprintln!("{e}"),
+    }
 }
