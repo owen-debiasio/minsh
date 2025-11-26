5331e0be80ccf3a4195feaf43164d18760af6da9
Owen <owen.debiasio@gmail.com>
2025-11-26 12:51:11 -0500

    Enhance cd function with empty check and fix cwd output
    Add check for empty directory in cd function and fix string literals.

--- src/fs.rs
+++ src/fs.rs
@@ -3,12 +3,17 @@ use std::env::{current_dir, set_current_dir};
 use crate::ENV;
 
 pub fn cd(dir: &str) {
+    if dir.trim().is_empty() {
+        return;
+    }
+
     let dir = match dir {
         "~" | "$HOME" => ENV.home.clone(),
         _ if dir.starts_with("~/") => format!("{}/{}", ENV.home, &dir[2..]),
         _ if dir.starts_with("$HOME/") => format!("{}/{}", ENV.home, &dir[6..]),
         _ => dir.to_string(),
     };
+
     if let Err(e) = set_current_dir(&dir) {
         eprintln!("cd: {dir}: {e}");
     }
@@ -18,7 +23,7 @@ pub fn cwd() -> String {
     let cwd = current_dir()
         .ok()
         .and_then(|p| p.to_str().map(String::from))
-        .unwrap_or("~".into());
+        .unwrap_or('~'.into());
     if cwd == ENV.home {
         "~".into()
     } else if let Some(r) = cwd.strip_prefix(&(ENV.home.clone() + "/")) {
